9c72897fc7ca76f0b2b886b28b7e8b4ee3c1b169
Navneeth <navneethmahadevan@gmail.com>
2025-12-14 04:45:27 +0530

    feat: standardize package name

--- launch/imu_publisher.launch.py
+++ launch/imu_publisher.launch.py
@@ -3,22 +3,21 @@ from launch_ros.actions import Node
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
-    params_file = PathJoinSubstitution([
-        FindPackageShare('imu_serial_reader'),
-        'config',
-        'params.yaml'
-    ])
-    
-    return LaunchDescription([
-        Node(
-            package='imu_serial_reader',
-            executable='imu_serial_node',
-            name='imu_serial_node',
-            output='screen',
-            parameters=[params_file],
-            remappings=[
-                ('imu/data_raw', 'imu/data_raw')
-            ]
-        )
-    ])
+    params_file = PathJoinSubstitution(
+        [FindPackageShare("ros2_imu_serial"), "config", "params.yaml"]
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package="ros2_imu_serial",
+                executable="imu_serial_node",
+                name="imu_serial_node",
+                output="screen",
+                parameters=[params_file],
+                remappings=[("imu/data_raw", "imu/data_raw")],
+            )
+        ]
+    )
--- setup.py
+++ setup.py
@@ -2,31 +2,31 @@ from setuptools import setup
 import os
 from glob import glob
 
-package_name = 'imu_serial_reader'
+package_name = "ros2_imu_serial"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), 
-         glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'config'),
-         glob('config/*.yaml')),  # Make sure this line exists
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (
+            os.path.join("share", package_name, "config"),
+            glob("config/*.yaml"),
+        ),  # Make sure this line exists
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Navneeth Mahadevan',
-    maintainer_email='navneethmahadevan@gmail.com',
-    description='ROS2 node for reading IMU data from serial port',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    maintainer="Navneeth Mahadevan",
+    maintainer_email="navneethmahadevan@gmail.com",
+    description="ROS2 node for reading IMU data from serial port",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'imu_serial_node = imu_serial_reader.imu_serial_node:main',
+        "console_scripts": [
+            "imu_serial_node = ros2_imu_serial.imu_serial_node:main",
         ],
     },
 )
